455e29524d0a8c49481ce4a18d5868fe48f07f41
Tim Mickel <tmickel@mit.edu>
2012-08-16 00:11:09 -0700

    Initial Django set-up

---
+++ web/web/settings/defaults.py
@@ -0,0 +1,60 @@
+import os.path
+
+PROJECT_MODULE = '.'.join(__name__.split('.')[:-2])
+
+def path(location):
+    return os.path.join(PROJECT_MODULE, location)
+
+TEMPLATE_DIRS = (
+    path('music/templates')
+)
+
+STATICFILES_DIRS = (
+    path('music/static'),
+)
+
+TIME_ZONE = 'America/New_York'
+
+LANGUAGE_CODE = 'en-us'
+
+USE_I18N = False
+
+USE_L10N = False
+
+USE_TZ = True
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'web.urls'
+
+WSGI_APPLICATION = 'web.wsgi.application'
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'south',
+
+    'web.music',
+)
