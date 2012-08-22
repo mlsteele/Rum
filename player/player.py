ec11d1f262225ac9e52799e79f5145d126f032ee
Isaac Gutekunst <igutekunst@gmail.com>
2012-08-21 19:10:58 -0700

    simpler protocal. Progress to client

--- player/player.py
+++ player/player.py
@@ -2,16 +2,27 @@ import zmq, hashlib, auth_pb2
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://rum.mit.edu:5000")
-SECRET = 'omgsecret'
+SECRET = 'pete'
 def auth():
     auth_message = auth_pb2.AuthMessage()
     auth_message.type = auth_pb2.AuthMessage.HELLO
-    hello = auth_pb2.HelloMessage()
-    auth_message.hello.name = "isaac"
+    auth_message.name = "isaac"
     socket.send(auth_message.SerializeToString())
 
     data = socket.recv()
-    response_auth_message = auth_pb2.AuthMessage()
-    response_auth_message.ParseFromString(data)
-    print response_auth_message
+    
+    challenge = auth_pb2.AuthMessage()
+    challenge.ParseFromString(data)
+    
+    if challenge.type != auth_pb2.AuthMessage.CHALLENGE:
+        raise Exception("Unexpected resposne from server")
+    else:
+        response = auth_pb2.AuthMessage()
+        response.response.response = hashlib.sha256(challenge.nonce + SECRET ).hexdigest()
+        socket.send(response.SerializeToString())
+
+    data = socket.recv()
+        
+    
+
 auth()
