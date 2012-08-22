fc43b83636fd7f694ee9b3518cc2c8ecf04aa1bc
Isaac Gutekunst <igutekunst@gmail.com>
2012-08-21 17:45:18 -0700

    auth protobuf files

--- player/player.py
+++ player/player.py
@@ -1,7 +1,19 @@
-import zmq
+import zmq, hashlib
 context = zmq.Context()
-socket = context.socket(zmq.PUB)
-socket.connect("tcp://127.0.0.1:5000")
-while True:
-    message = raw_input("Message: ")
-    socket.send("update:" + message)
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://rum.mit.edu:5000")
+SECRET = 'omgsecret'
+def auth():
+    socket.send("auth#isaac")
+
+    challenge = socket.recv().split("#")
+    if challenge[0] == "challenge":
+
+        response = "response#" + challenge[1] + "#" + hashlib.sha256(challenge[1] +
+                                                SECRET).hexdigest()
+        socket.send(response)
+        print socket.recv()
+
+
+
+auth()
